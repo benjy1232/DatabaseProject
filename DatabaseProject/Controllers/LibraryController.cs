e81c7962c13ac7f42c139b7bc0ce5a4b65bef38e
Ben Serrano <b.serrano8970@gmail.com>
2024-04-12 10:48:12 -0500

    Finish Adding Update/Add/Delete for Songs on the main page

--- DatabaseProject/Controllers/LibraryController.cs
+++ DatabaseProject/Controllers/LibraryController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MySql.Data.MySqlClient;
-using Org.BouncyCastle.Cms;
 
 namespace DatabaseProject.Controllers;
 
@@ -54,8 +53,8 @@ public class LibraryController(ILogger<LibraryController> logger, IConfiguration
             using var reader = command.ExecuteReader();
             while (reader.Read())
             {
-                string genre = reader[4].ToString() ?? "N/A";
-                genre = genre.Length == 0 ? "N/A" : genre;
+                // Genre should be N/A when string is null or empty - otherwise return the value of reader[4].ToString()
+                string genre = string.IsNullOrEmpty(reader[4].ToString()) ? "N/A" : reader[4].ToString()!;
                 result += $"""
                            <tr>
                              <td>{reader[0]}</td>
@@ -134,6 +133,68 @@ public class LibraryController(ILogger<LibraryController> logger, IConfiguration
         return RedirectToAction("Index");
     }
 
+    public IActionResult UpdateSong(uint songId, string? songName, uint? albumId, uint? artistId, ushort? genreId, bool updateGenre)
+    {
+        try
+        {
+            using var connection = new MySqlConnection(configuration.GetConnectionString("mySqlConn"));
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = "SELECT song_title, album_id, artist_id, genre_id FROM Song WHERE id = @id";
+            command.Parameters.AddWithValue("@id", songId);
+            command.Prepare();
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                songName = string.IsNullOrEmpty(songName) ? reader[0].ToString()! : songName;
+                albumId ??= reader.GetUInt32(1);
+                artistId ??= reader.GetUInt32(2);
+                genreId ??= reader.GetUInt16(3);
+            }
+            reader.Close();
+
+
+            string commandStr;
+            if (updateGenre)
+            {
+                commandStr = """
+                             UPDATE Song
+                             SET song_title = @songName, album_id = @albumId, artist_id = @artistId, genre_id = @genreId
+                             WHERE id = @id
+                             """;     
+            }
+            else
+            {
+                commandStr = """
+                             UPDATE Song
+                             SET song_title = @songName, album_id = @albumId, artist_id = @artistId
+                             WHERE id = @id
+                             """;
+            }
+
+            command.CommandText = commandStr;
+            command.Parameters.AddWithValue("@songName", songName);
+            command.Parameters.AddWithValue("@albumId", albumId);
+            command.Parameters.AddWithValue("@artistId", artistId);
+            if (updateGenre)
+                command.Parameters.AddWithValue("@genreId", genreId);
+
+            command.ExecuteNonQuery();
+            logger.LogInformation("Command Successful");
+        }
+        catch (MySqlException ex)
+        {
+            if (ex.Number == 1065)
+            {
+                logger.LogError($"Entry {songName} - {albumId} already exists");
+                return RedirectToAction("Index");
+            }
+            logger.LogError($"Error {ex.Number} has occurred: {ex.Message}");
+            throw;
+        }
+        return RedirectToAction("Index");
+    }
+
     private void CreateAllLists()
     {
         try
@@ -146,15 +207,29 @@ public class LibraryController(ILogger<LibraryController> logger, IConfiguration
 
             using var albumCmd = connection.CreateCommand();
             albumCmd.CommandText = """
-                                   SELECT id, album_name, A.artist_name FROM Album
+                                   SELECT Album.id, album_name, A.artist_name FROM Album
+                                   INNER JOIN Artist A ON A.id = artist_id
                                    """;
             using var albumReader = albumCmd.ExecuteReader();
             while (albumReader.Read())
             {
                 albums.Add(new SelectListItem($"{albumReader[2]} - {albumReader[1]}", albumReader[0].ToString()));
             }
+            albumReader.Close();
 
             ViewBag.Albums = albums;
+
+            List<SelectListItem> songs = [];
+            using var songCmd = connection.CreateCommand();
+            songCmd.CommandText = """
+                                   SELECT Song.id, song_title, A.album_name FROM Song
+                                   INNER JOIN Album A ON A.id = album_id
+                                   """;
+            using var songReader = songCmd.ExecuteReader();
+            while (songReader.Read())
+            {
+                songs.Add(new SelectListItem($"{songReader[1]} - {songReader[2]}", songReader[0].ToString()));
+            }            ViewBag.Songs = songs;
         }
         catch (MySqlException ex)
         {
