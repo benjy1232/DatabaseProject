8bfb010d61c527d36f02b4a84b0f46f0cf82a9a3
Ben Serrano <b.serrano8970@gmail.com>
2024-04-11 20:07:55 -0500

    Add WHERE to SELECT query for Library
    Added the ability to use WHERE in SELECT statements when on
    /Library/Index/{artistName}
    
    Fixed Prepared SQL statements that would have caused issues

--- DatabaseProject/Controllers/LibraryController.cs
+++ DatabaseProject/Controllers/LibraryController.cs
@@ -9,27 +9,44 @@ public class LibraryController(ILogger<LibraryController> logger, IConfiguration
     // GET
     public IActionResult Index()
     {
-        CreateSongsTable();
+        CreateSongsTable(null);
         return View();
     }
 
-    private void CreateSongsTable()
+    [Route("Library/Index/{artistName}")]
+    public IActionResult Index(string artistName)
+    {
+        CreateSongsTable(artistName);
+        return View();
+    }
+
+    private void CreateSongsTable(string? artistName)
     {
         string result = "";
         // Okay using raw SQL here bc no user input is needed - no security issue here
-        string commandString = """
-                               SELECT song_title, A.album_name, A.release_year, M.artist_name, G.name, AA.artist_name FROM Song
-                               INNER JOIN Album A ON A.id = album_id
-                               INNER JOIN Artist M ON M.id = Song.artist_id
-                               INNER JOIN Artist AA ON AA.id = A.artist_id
-                               LEFT JOIN Genre G ON G.id = Song.genre_id
-                               """;
         try
         {
             using var connection = new MySqlConnection(configuration.GetConnectionString("mySqlConn"));
             connection.Open();
+            string commandString = """
+                                   SELECT song_title, A.album_name, A.release_year, M.artist_name, G.name, AA.artist_name FROM Song
+                                   INNER JOIN Album A ON A.id = album_id
+                                   INNER JOIN Artist M ON M.id = Song.artist_id
+                                   INNER JOIN Artist AA ON AA.id = A.artist_id
+                                   LEFT JOIN Genre G ON G.id = Song.genre_id
+                                   """;
             using var command = connection.CreateCommand();
-            command.CommandText = commandString;
+            if (!string.IsNullOrEmpty(artistName))
+            {
+                commandString += " WHERE AA.artist_name = @artistName OR M.artist_name = @artistName";
+                command.CommandText = commandString;
+                command.Parameters.AddWithValue("artistName", artistName);
+                command.Prepare();
+            }
+            else
+            {
+                command.CommandText = commandString;
+            }
             using var reader = command.ExecuteReader();
             while (reader.Read())
             {
@@ -52,10 +69,9 @@ public class LibraryController(ILogger<LibraryController> logger, IConfiguration
         catch (MySqlException ex)
         {
             logger.LogError($"Error {ex.Number} has occurred: {ex.Message}");
+            throw;
         }
 
         ViewBag.Table = result;
     }
-
-
 }
